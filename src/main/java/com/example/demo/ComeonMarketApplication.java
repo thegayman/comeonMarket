295468c3c414d8857a2b41d6a18fd8b8e781d505
物以类聚_LanCho <_LanCho@PC-201612041322>
2017-08-03 19:45:22 +0800

    5555

--- src/main/java/com/example/demo/ComeonMarketApplication.java
+++ src/main/java/com/example/demo/ComeonMarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComeonMarketApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(ComeonMarketApplication.class, args);
 	}
